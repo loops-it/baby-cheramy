8a1e71be91c4fc5a982b023c4abe74914ce7bc12
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-05-16 09:46:22 +0530

    path added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const indexRouter = require('./routes/index');
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
